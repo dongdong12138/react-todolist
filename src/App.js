f56c4b7485f7a9a4281b3ba531665ad8dc76b438
BarryDong <286084845@qq.com>
2022-12-26 21:04:58 +0800

    将App.js中的函数式组件改成类式组件

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
+import React, { Component } from 'react'
 import Header from './components/Header'
 import List from './components/List'
 import Footer from './components/Footer'
 import './App.css'
 
-function App() {
-  return (
-    <div className="todo-container">
-      <div className="todo-wrap">
-        <Header />
-        <List />
-        <Footer />
+class App extends Component {
+  render() {
+    return (
+      <div className="todo-container">
+        <div className="todo-wrap">
+          <Header/>
+          <List/>
+          <Footer/>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default App
