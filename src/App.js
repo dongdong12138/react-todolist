466e5c2e05be79161975bb739bb99e4684dad6a9
BarryDong <286084845@qq.com>
2022-12-26 20:41:16 +0800

    拆分Footer组件结构

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import List from './components/List'
+import Footer from './components/Footer'
 import './App.css'
 
 function App() {
@@ -8,15 +9,7 @@ function App() {
       <div className="todo-wrap">
         <Header />
         <List />
-        <div className="todo-footer">
-          <label>
-            <input type="checkbox"/>
-          </label>
-          <span>
-            <span>已完成0</span> / 全部2
-          </span>
-          <button className="btn btn-danger">清除已完成任务</button>
-        </div>
+        <Footer />
       </div>
     </div>
   )
