9ac31cf7d32dfac7f15c8877bd99d4bda3a02d4f
BarryDong <286084845@qq.com>
2022-12-26 20:43:51 +0800

    拆分Item组件结构

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -3,9 +3,13 @@ import React, { Component } from 'react'
 class Item extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <li>
+        <label>
+          <input type="checkbox"/>
+          <span>吃饭</span>
+        </label>
+        <button className="btn btn-danger" style={{display: 'none'}}>删除</button>
+      </li>
     )
   }
 }
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,23 +1,11 @@
 import React, { Component } from 'react'
+import Item from '../Item'
 
 class List extends Component {
   render() {
     return (
       <ul className="todo-main">
-        <li>
-          <label>
-            <input type="checkbox"/>
-            <span>吃饭</span>
-          </label>
-          <button className="btn btn-danger" style={{display: 'none'}}>删除</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox"/>
-            <span>睡觉</span>
-          </label>
-          <button className="btn btn-danger" style={{display: 'none'}}>删除</button>
-        </li>
+        <Item />
       </ul>
     )
   }
