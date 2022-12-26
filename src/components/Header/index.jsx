e858a08bcad60f9d523affacb851d735e4a563f7
BarryDong <286084845@qq.com>
2022-12-26 21:35:15 +0800

    Header组件的input绑定keyup事件

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,10 +2,14 @@ import React, { Component } from 'react'
 import './index.css'
 
 class Header extends Component {
+  handleKeyUp = (event) => {
+    if (event.keyCode !== 13) return
+    console.log(event.target.value)
+  }
   render() {
     return (
       <div className="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认"/>
+        <input type="text" placeholder="请输入你的任务名称，按回车键确认" onKeyUp={this.handleKeyUp}/>
       </div>
     )
   }
