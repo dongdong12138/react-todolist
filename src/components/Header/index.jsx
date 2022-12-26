8e8c6771bc62e37d7a5ae3aa449132cb005fa2ca
BarryDong <286084845@qq.com>
2022-12-26 22:10:50 +0800

    1.给input的keyUp事件添加限制条件； 2.添加todo后清空输入框。

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,9 +4,13 @@ import './index.css'
 
 class Header extends Component {
   handleKeyUp = (event) => {
+    let { value } = event.target
+    value = value.trim()
     if (event.keyCode !== 13) return
-    const todo = {id: nanoid(), name: event.target.value, done: false}
+    if (!value) return
+    const todo = {id: nanoid(), name: value, done: false}
     this.props.addTodo(todo)
+    event.target.value = ''
   }
   render() {
     return (
