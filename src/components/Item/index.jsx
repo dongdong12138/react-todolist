13d83ccf3a5e9ae190c875cdee0a00d33bd529c0
BarryDong <286084845@qq.com>
2022-12-27 10:01:16 +0800

    每一个Item鼠标悬浮时高亮背景色并显示删除按钮

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,15 +2,25 @@ import React, { Component } from 'react'
 import './index.css'
 
 class Item extends Component {
+  state = {mouse: false}
+
+  handleMouse = (flag) => {
+    return () => {
+      this.setState({mouse: flag})
+    }
+  }
+
   render() {
+    const {mouse} = this.state
     const {name, done} = this.props
     return (
-      <li>
+      <li style={{backgroundColor: mouse ? '#ddd' : ''}} onMouseEnter={this.handleMouse(true)}
+          onMouseLeave={this.handleMouse(false)}>
         <label>
           <input type="checkbox" defaultChecked={done}/>
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{display: 'none'}}>删除</button>
+        <button className="btn btn-danger" style={{display: mouse ? 'block' : 'none'}}>删除</button>
       </li>
     )
   }
